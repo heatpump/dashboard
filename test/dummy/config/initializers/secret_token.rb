11dbe82b28c524ab3e6b112ed46a44cd0cfc0303
Jun Komatsu <jun@uniba.jp>
2013-05-26 18:25:02 +0900

    first commit with generated rails engine files

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '5a79633d30f44a893f45c845666ca9e7158792092e4c8f0a2f08babdd3073a510b4d8f18aa4d46cfc32e4a167acd98adc49033ce3776986b9627c4897e64b69d'
